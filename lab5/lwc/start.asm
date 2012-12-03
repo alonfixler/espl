308469e1fe2a61dcec87df74a44b033c09fdec62
Alon Fixler <alonfixler@gmail.com>
2012-12-03 15:46:06 +0200

    bla

--- lab5/lwc/start.asm
+++ lab5/lwc/start.asm
@@ -10,14 +10,28 @@ global strlen
 
 extern main
 _start:
+	lea ecx,[esp+4]
+	push ecx
+	push DWORD [esp+4]
 	call	main
         mov     ebx,eax
 	mov	eax,1
 	int 0x80
-
-print:
+      
 read:
+      mov eax,3
+      mov ebx,[esp+4]
+      mov ecx,[esp+8]
+      mov edx,[esp+12]
+      int 0x80
+      ret
 write:
+      mov eax,4
+      mov ebx,[esp+4]
+      mov ecx,[esp+8]
+      mov edx,[esp+12]
+      int 0x80
+      ret
 open:
       mov eax,5
       mov ebx,[esp+4]
@@ -26,5 +40,21 @@ open:
       int 0x80
       ret
 close:
+      mov eax,6
+      mov ebx,[esp+4]
+      int 0x80
+      ret
 strlen:
-	ret
+      mov ecx,0
+loop:
+      mov edx,[esp+8]
+      movzx edx,byte [edx]
+      test edx,edx
+      je end
+      add ecx,1
+      add DWORD [edx],1 
+      jmp loop
+      
+end:
+      mov eax,ecx
+      ret
