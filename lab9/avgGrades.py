3c4322bf680f91b26035de7a6fcca043e26886ee
Alon Fixler <alonfixler@gmail.com>
2013-01-02 18:43:23 +0200

    bla

--- lab9/avgGrades.py
+++ lab9/avgGrades.py
@@ -1,1 +1,11 @@
- 
+import sys
+lineCount = 0
+grades = 0
+f = open(sys.argv[1],'r')
+f.next()
+for line in f:
+  a = line.split(',')
+  grades = grades + int(a[2])
+  lineCount = lineCount + 1
+print "The avg is: "+str(float(grades)/float(lineCount))
+f.close()
