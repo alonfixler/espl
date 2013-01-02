6fd0456036f79ae98662536b8bed8800fa64848b
Alon Fixler <alonfixler@gmail.com>
2013-01-02 16:37:18 +0200

    a

--- lab8/esplsh/esplsh.c
+++ lab8/esplsh/esplsh.c
@@ -101,30 +101,73 @@ void run_program() {
   /* TODO: pipelines */
   /* TODO: background commands */
 
-  if((pid=fork())>0) {
-    
-    while(*argv)
+   int ind=0;
+   char **t = argv;
+   while(*t)
     {
-      if(**argv=='|')
+      if(**t=='|')
       {
-	
+	ind=1;
+	*t = 0;
+	t++;
+	int pid;
+	int fd[2];
+	pipe(fd);
+	pid=fork();
+	if(pid==0) 
+	{
+	  close(fd[0]);
+	  dup2(fd[1],1);
+	  close(fd[1]);
+	  execvp(argv[0],argv);
+	} 
+	else 
+	{
+	  pid=fork();
+	  if(pid==0) 
+	  {
+	    close(fd[1]);
+	    dup2(fd[0] , 0);
+	    close(fd[0]);
+	    execvp(*t , t);
+	  }
+	  else
+	  {
+	    close(fd[0]);
+	    close(fd[1]);
+	    waitpid(pid, &status, 0);
+	  }
+	}
       }
+      t++;
     }
+  if(ind==1) 
+    return;
+  
+  if((pid=fork())>0) {
     waitpid(pid, &status, 0);
     sprintf(ststr, "%d", status);
     setenv("?", ststr, 1);
-  } else if(pid==0) {
-    if(argc>=3 && *argv[argc-2]== '>')
-    {
-      int fd = open(argv[argc-1],O_WRONLY|O_CREAT|O_TRUNC,0644);
-      dup2(fd,1);
-      close(fd);
-      argv[argc-2] = NULL;
-    }
-   
-    execvp(argv[0], argv);
-    perror(argv[0]);
-  } else {
+  } 
+  else if(pid==0) {
+      if(argc>=3 && *argv[argc-2]== '>')
+      {
+	int fd = open(argv[argc-1],O_WRONLY|O_CREAT|O_TRUNC,0644);
+	dup2(fd,1);
+	close(fd);
+	argv[argc-2] = NULL;
+      }
+      else if(argc>=3 && *argv[argc-2]== '<')
+      {
+	int fd = open(argv[argc-1],O_WRONLY|O_CREAT|O_TRUNC,0644);
+	dup2(fd,0);
+	close(fd);
+	argv[argc-2]=NULL; 
+      }
+      execvp(argv[0], argv);
+      perror(argv[0]);
+  } 
+  else {
     perror(getenv("SHELL")); /* problem while forking, not due to a particular program */
   }
 }
